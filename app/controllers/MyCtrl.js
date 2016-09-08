bbec216e36ec4d054f70b2b39a5504be959b0e01
Matt Hamil <j.matthew.hamil@gmail.com>
2016-09-08 11:28:15 -0500

    Angular boilerplate

---
+++ app/controllers/MyCtrl.js
@@ -0,0 +1,5 @@
+'use strict';
+
+app.controller('MyCtrl', function($scope) {
+  $scope.message = 'Hello world!';
+});
---
+++ app/factories/MyFactory.js
@@ -0,0 +1,8 @@
+'use strict';
+
+app.factory('MyFactory', () => {
+
+  return {
+
+  };
+});
