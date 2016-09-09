ffbc75a5534103f597262bfc225a20d0da9aeb12
Delaine Wendling <delaine.wendling@gmail.com>
2016-09-09 12:46:05 -0500

    Added Ron Swanson quotes to 'home screen

---
+++ app/controllers/QuotesCtrl.js
@@ -0,0 +1,9 @@
+"use strict";
+
+app.controller("QuotesCtrl", function($scope, ApiFactory){
+  ApiFactory.getQuotes()
+  .then((quotes)=>{
+    $scope.quotes = quotes;
+    console.log("$scope.quotes");
+  })
+});
